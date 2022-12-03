75168cfce062bb7541c84ebc28d5b7ca85ac0455
Nicole Campbell <nicoca@umich.edu>
2022-12-02 19:34:15 -0500

    updated cbf for i and j contribution

--- SingleIntegrator.py
+++ SingleIntegrator.py
@@ -1,11 +1,15 @@
+from colorsys import hsv_to_rgb
 import numpy as np 
 
 class SingleIntegrator:
-	def __init__(self, x0, r, G):
+	def __init__(self, x0, r, G, alpha = 10, d_thresh = 1, u = np.array([[0],[0],[0]])):
 		self.X0 = x0
 		self.X = x0
 		self.G = G
 		self.R = r
+		self.alpha = alpha
+		self.d_thresh = d_thresh
+		self.u = u
 
 	def lyapunov(self):
 		V = np.linalg.norm(self.X[-3:] -  self.G)**2
@@ -22,9 +26,25 @@ class SingleIntegrator:
 	def agent_barrier(self, r, X):
 		# r: radius of other agent
 		# X: coordinates of other agent
-		h = np.linalg.norm(self.X[-3:] - X[-3:])**2 - (self.R + r)**2
-		dh = 2 * (self.X[-3:] - X[-3:]).T
-		return h, dh
+		self.h = np.linalg.norm(self.X[-3:] - X[-3:])**2 - (self.R + r)**2
+		self.dhi = 2 * ( self.X[-3:] - X[-3:] ).T # wrt to ego agent
+		self.dhj = -2 * ( self.X[-3:] - X[-3:] ).T # wrt to other agent
+
+		return self.h, self.dhi, self.dhj
+
+	def agent_alpha(self, uj, ui_best): #, nomj):
+		# uj: velocity input of other agent
+		# dhj: dh_dx for other agent
+		# dhi: dh_dx for ego agent
+		# hi: h for ego agent
+		# nomj: nominal direction of other agent
+		# rho_d = np.tanh( self.dhj @ uj - ( -self.alpha * self.h - self.dhi @ ui_best) ) - np.tanh( self.d_thresh )
+		# theta1 = np.acos( (dhi * uj) / (np.norm(dhi) * np.norm(uj)) ) #  angle between surface normal and actual agent trajectory
+		# theta2 = np.acos( (dhi * nomj) / (np.norm(dhi) * np.norm(nomj)) ) #  angle between surface normal and nominal agent trajectory
+		# rho_theta = np.tanh( theta2 / theta1 )
+		# self.alpha = rho_d #* rho_theta
+		# return rho_d[0][0]
+		return 2
 
 
 
--- multi_agent.py
+++ multi_agent.py
@@ -9,35 +9,33 @@ from matplotlib.animation import FuncAnimation
 
 # time info
 T = 10
-dt = 0.01
+dt = 0.001
 t_steps = int(T/dt)
 
-# environment info 
-XC = np.array([[5],[5],[5]]) # obstacle center
-R = 1  # obstacle radius
-
 # define cooperative robots
+alpha_init = 1
 c_robots = []
-c_robots.append(SI(np.array([[0],[0],[0]]), 1, np.array([[10],[10],[11]])))
-c_robots.append( SI(np.array([[10],[0],[0]]), 1, np.array([[0],[10],[11]])))
-c_robots.append( SI(np.array([[10],[3],[9]]), 1, np.array([[0],[3],[9]])))
-c_robots.append( SI(np.array([[10],[8],[10]]), 1, np.array([[0],[8],[10]])))
-c_robots.append( SI(np.array([[0],[4],[2]]), 1, np.array([[10],[4],[2]])))
+# c_robots.append(SI(np.array([[0],[0],[0]]), 1, np.array([[10],[10],[11]]),alpha_init))
+# c_robots.append( SI(np.array([[10],[0],[0]]), 1, np.array([[0],[10],[11]]),alpha_init))
+# c_robots.append( SI(np.array([[10],[3],[9]]), 1, np.array([[0],[3],[9]]),alpha_init))
+# c_robots.append( SI(np.array([[10],[8],[10]]), 1, np.array([[0],[8],[10]]),alpha_init))
+c_robots.append( SI(np.array([[0],[7],[5]]), 1, np.array([[10],[7],[5]]),alpha_init))
 num_c_robots = len(c_robots)
 
 
 # define uncooperative robots
 uc_robots = []
-uc_robots.append( SI(np.array([[10],[10],[0]]), 1, np.array([[1],[0],[12]])))
-uc_robots.append( SI(np.array([[2],[10],[5]]), 1, np.array([[2],[0],[5]])))
-uc_robots.append( SI(np.array([[8],[10],[5]]), 1, np.array([[8],[0],[5]])))
-uc_robots.append( SI(np.array([[10],[5],[8]]), 1, np.array([[0],[5],[8]])))
-uc_robots.append( SI(np.array([[4],[0],[2]]), 1, np.array([[4],[10],[2]])))
+# uc_robots.append( SI(np.array([[10],[10],[0]]), 1, np.array([[1],[0],[12]]),alpha_init))
+uc_robots.append( SI(np.array([[6],[10],[5]]), 1, np.array([[6],[0],[5]]),alpha_init))
+uc_robots.append( SI(np.array([[8],[10],[5]]), 1, np.array([[8],[0],[5]]),alpha_init))
+# uc_robots.append( SI(np.array([[10],[5],[8]]), 1, np.array([[0],[5],[8]]),alpha_init))
+# uc_robots.append( SI(np.array([[4],[0],[2]]), 1, np.array([[4],[10],[2]]),alpha_init))
 num_uc_robots = len(uc_robots)
 
 # define adveserial robots
 a_robots = []
-a_robots.append( SI(np.array([[0],[0],[0]]), 1, c_robots[0].X))
+# a_robots.append( SI(np.array([[0],[0],[0]]), 1, uc_robots[0].X, alpha_init))
+a_robots.append( SI(np.array([[0],[10],[5]]), 1, c_robots[0].X, alpha_init))
 num_a_robots = len(a_robots)
 
 # set up control for uncooperative agents
@@ -45,23 +43,17 @@ u1 = cp.Variable((3,1))
 delta1 = cp.Variable(1)
 V1 = cp.Parameter()
 dV1_dx = cp.Parameter((1,3))
-h1 = cp.Parameter()
-dh1_dx = cp.Parameter((1,3))
 P = np.identity(3)
 
-alpha1 = 10
+alpha1 = 15
 k1 = 1
 
 V1.value = uc_robots[0].lyapunov()[0] 
 dV1_dx.value = uc_robots[0].lyapunov()[1]
 
-h1.value = uc_robots[0].sphere_barrier(R, XC)[0]
-dh1_dx.value = uc_robots[0].sphere_barrier(R, XC)[1]
-
 objective1 = cp.Minimize(10 * cp.quad_form(u1,P) + 10 * cp.square(delta1))
 constraint1 = [ ]
 constraint1 += [ dV1_dx @ u1 <= - k1 * V1 + delta1 ] # CLF
-constraint1 += [ dh1_dx @ u1 >= - alpha1 * h1 ] # CBF for obstacle
 prob1 = cp.Problem(objective1, constraint1)
 
 # set up control for cooperative agents
@@ -69,44 +61,64 @@ u2 = cp.Variable((3,1))
 delta2 = cp.Variable(1)
 V2 = cp.Parameter()
 dV2_dx = cp.Parameter((1,3))
-h2 = cp.Parameter()
-dh2_dx = cp.Parameter((1,3))
+
 h2_a = []
-dh2_dx_a = []
+dhi2_dx_a = []
+dhj2_dx_a = []
+alpha2 = []
+xj_dot = []
 
 for i in range( num_uc_robots + num_c_robots + num_a_robots - 1 ):
 	h2_a.append(cp.Parameter())
-	dh2_dx_a.append(cp.Parameter((1,3)))
+	dhi2_dx_a.append(cp.Parameter((1,3)))
+	dhj2_dx_a.append(cp.Parameter((1,3)))
+	alpha2.append(15) # alpha2.append(cp.Parameter())
+	xj_dot.append(cp.Parameter((3,1)))
 
-alpha2 = 10
-k2 = 3
+k2 = 3 
 
 V2.value = c_robots[0].lyapunov()[0] 
 dV2_dx.value = c_robots[0].lyapunov()[1] 
 
-h2.value = c_robots[0].sphere_barrier(R, XC)[0] 
-dh2_dx.value = c_robots[0].sphere_barrier(R, XC)[1]
 
 objective2 = cp.Minimize(10 * cp.quad_form(u2,P) + 10 * cp.square(delta2))
 constraint2 = [ ]
 constraint2 += [ dV2_dx @ u2 <= - k2 * V2 + delta2 ] # CLF
-constraint2 += [ dh2_dx @ u2 >= - alpha2 * h2 ] # CBF for obstacle
 
 for i in range( num_uc_robots + num_c_robots - 1 ):
-	constraint2 += [ dh2_dx_a[i] @ u2 >= - alpha2 * h2_a[i] ] # CBF for other agents
+	constraint2 += [ dhi2_dx_a[i] @ u2 + dhj2_dx_a[i] @ xj_dot[i] >= - alpha2[i] * h2_a[i] ] # CBF for other agents
 
-prob2 = cp.Problem(objective2, constraint2)
+prob2 = cp.Problem(objective2, constraint2) 
+
+# set up control for best case cooperative agent
+# u_b = cp.Variable((3,1))
+# Q = cp.Parameter((1,3))
+
+# h_b = []
+# dhi_dx_b = []
+# dhj_dx_b = []
+# alpha_b = []
+# xj_dot_b = []
+
+# for i in range( num_uc_robots + num_c_robots + num_a_robots - 1 ):
+# 	h_b.append(cp.Parameter())
+# 	dhi_dx_b.append(cp.Parameter((1,3)))
+# 	dhj_dx_b.append(cp.Parameter((1,3)))
+# 	alpha_b.append(cp.Parameter())
+# 	xj_dot_b.append(cp.Parameter((3,1)))
+
+# objective_b = cp.Maximize( Q @ u_b )
+# constraint_b = [ ]
+
+# for i in range( num_uc_robots + num_c_robots - 1 ):
+# 	constraint_b += [ dhi_dx_b[i] @ u_b + dhj_dx_b[i] @ xj_dot_b[i] >= - alpha_b[i] * h_b[i] ] # CBF for other agents
+
+# prob_b = cp.Problem(objective_b, constraint_b) 
 
 # plot setup
 plt.ion()
 fig = plt.figure()
 ax = plt.axes(projection='3d')
-
-u, v = np.mgrid[0:2*np.pi:50j, 0:np.pi:50j]
-x = R * np.cos(u)*np.sin(v)
-y = R * np.sin(u)*np.sin(v)
-z = R * np.cos(v)
-ax.plot_surface(x + XC[0], y + XC[1], z + XC[2], color = 'orange')
 
 # update dynamics
 for t in range( t_steps ):
@@ -116,65 +128,97 @@ for t in range( t_steps ):
 		V1.value = uc_robots[i].lyapunov()[0] 
 		dV1_dx.value = uc_robots[i].lyapunov()[1]
 
-		h1.value = uc_robots[i].sphere_barrier(R, XC)[0] 
-		dh1_dx.value = uc_robots[i].sphere_barrier(R, XC)[1] 
-
-		prob1.solve()
-		# print("status of prob 1: ", prob1.status)
+		prob1.solve() 
 
 		X_next = uc_robots[i].X[-3:] + u1.value * dt
 		uc_robots[i].X = np.concatenate((uc_robots[i].X, X_next), axis = 0)
+		uc_robots[i].u = u1.value
+
+	for i in range( num_a_robots ):
+
+		a_robots[i].G = c_robots[0].X[-3:]
+		
+		V1.value = a_robots[i].lyapunov()[0] 
+		dV1_dx.value = a_robots[i].lyapunov()[1]
+
+		prob1.solve()
+
+		X_next = a_robots[i].X[-3:] + u1.value * dt
+		a_robots[i].X = np.concatenate((a_robots[i].X, X_next), axis = 0)
+		a_robots[i].u = u1.value
 
 	for j in range( num_c_robots ):
 
 		V2.value = c_robots[j].lyapunov()[0] 
 		dV2_dx.value = c_robots[j].lyapunov()[1] 
 
-		h2.value = c_robots[j].sphere_barrier(R, XC)[0]
-		dh2_dx.value = c_robots[j].sphere_barrier(R, XC)[1]
-
 		n = 0 # n-th robot
 
-		for k in range( num_uc_robots ):
+		for k in range( num_uc_robots ):  # uj, dhj, dhi, hi
 			h2_a[n].value = c_robots[j].agent_barrier(uc_robots[k].R, uc_robots[k].X)[0] 
-			dh2_dx_a[n].value = c_robots[j].agent_barrier(uc_robots[k].R, uc_robots[k].X)[1] 
-			n = n + 1
+			dhi2_dx_a[n].value = c_robots[j].agent_barrier(uc_robots[k].R, uc_robots[k].X)[1] 
+			dhj2_dx_a[n].value = c_robots[j].agent_barrier(uc_robots[k].R, uc_robots[k].X)[2] 
+			xj_dot[n].value = uc_robots[k].u
+
+			# h_b = c_robots[j].agent_barrier(uc_robots[k].R, uc_robots[k].X)[0] 
+			# dhi_dx_b[n].value = c_robots[j].agent_barrier(uc_robots[k].R, uc_robots[k].X)[1] 
+			# dhj_dx_b[n].value = c_robots[j].agent_barrier(uc_robots[k].R, uc_robots[k].X)[2] 
+			# xj_dot_b[n].value = uc_robots[k].u
+			# Q.value = dhi_dx_b[n].value
+			# prob_b.solve()
+
+			# alpha2[n].value = c_robots[j].agent_alpha(uc_robots[k].u,u_b.value) 
+
+			n = n + 1 
 
 		for k in range( num_c_robots ):
+
 			if k == j:
 				continue
-			h2_a[n].value = c_robots[j].agent_barrier(uc_robots[k].R, c_robots[k].X)[0]  
-			dh2_dx_a[n].value = c_robots[j].agent_barrier(c_robots[k].R, c_robots[k].X)[1]
+
+			h2_a[n].value = c_robots[j].agent_barrier(c_robots[k].R, c_robots[k].X)[0]  
+			dhi2_dx_a[n].value = c_robots[j].agent_barrier(c_robots[k].R, c_robots[k].X)[1]
+			dhj2_dx_a[n].value = c_robots[j].agent_barrier(c_robots[k].R, c_robots[k].X)[2]
+			xj_dot[n].value = c_robots[k].u
+
+			# h_b = c_robots[j].agent_barrier(c_robots[k].R, c_robots[k].X)[0]  
+			# dhi_dx_b[n].value = c_robots[j].agent_barrier(c_robots[k].R, c_robots[k].X)[1]
+			# dhj_dx_b[n].value = c_robots[j].agent_barrier(c_robots[k].R, c_robots[k].X)[2]
+			# xj_dot_b[n].value = c_robots[k].u
+			# Q.value = dhi_dx_b[n].value
+			# prob_b.solve()
+
+			# alpha2[n].value = c_robots[j].agent_alpha(c_robots[k].u, u_b.value) # alpha_dot
+			
 			n = n + 1
 
 		for k in range( num_a_robots ):
+
 			if k == j:
 				continue
+
 			h2_a[n].value = c_robots[j].agent_barrier(a_robots[k].R, a_robots[k].X)[0]  
-			dh2_dx_a[n].value = c_robots[j].agent_barrier(a_robots[k].R, a_robots[k].X)[1]
+			dhi2_dx_a[n].value = c_robots[j].agent_barrier(a_robots[k].R, a_robots[k].X)[1]
+			dhj2_dx_a[n].value = c_robots[j].agent_barrier(a_robots[k].R, a_robots[k].X)[2]
+			xj_dot[n].value = a_robots[k].u
+
+			# h_b = c_robots[j].agent_barrier(a_robots[k].R, a_robots[k].X)[0]  
+			# dhi_dx_b[n].value = c_robots[j].agent_barrier(a_robots[k].R, a_robots[k].X)[1]
+			# dhj_dx_b[n].value = c_robots[j].agent_barrier(a_robots[k].R, a_robots[k].X)[2]
+			# xj_dot_b[n].value = a_robots[k].u
+			# Q.value = dhi_dx_b[n].value
+			# prob_b.solve()
+			# alpha2[n].value = c_robots[j].agent_alpha(a_robots[k].u,u_b.value) 
+
 			n = n + 1
 
 		prob2.solve()
-		# print("status of prob 2: ", prob2.status)
 
 		X_next = c_robots[j].X[-3:] + u2.value * dt
 		c_robots[j].X = np.concatenate((c_robots[j].X, X_next), axis = 0)
+		c_robots[j].u = u2.value
 
-		for i in range( num_a_robots ):
 
-			a_robots[i].G = c_robots[0].X[-3:]
-			
-			V1.value = a_robots[i].lyapunov()[0] 
-			dV1_dx.value = a_robots[i].lyapunov()[1]
-
-			h1.value = a_robots[i].sphere_barrier(R, XC)[0] 
-			dh1_dx.value = a_robots[i].sphere_barrier(R, XC)[1] 
-
-			prob1.solve()
-			# print("status of prob 1: ", prob1.status)
-
-			X_next = a_robots[i].X[-3:] + u1.value * dt
-			a_robots[i].X = np.concatenate((a_robots[i].X, X_next), axis = 0)
 
 	for j in range( num_c_robots ):
 		ax.scatter3D(c_robots[j].X[::3], c_robots[j].X[1::3] , c_robots[j].X[2::3], color = "green")
@@ -188,9 +232,10 @@ for t in range( t_steps ):
 	plt.xlabel("x")
 	plt.ylabel("y")
 	plt.draw()
-	plt.pause(0.0001)
+	plt.pause(0.000001)
 	ax.cla()
 	# plt.show()
 
 plt.ioff
-plt.close
+plt.close('all')
+plt.close(fig)
